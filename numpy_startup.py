fb774c916fd93bf3e13c5fefabad52e2f058059b
william <espiw@farmingdale.edu>
2025-11-05 20:12:39 -0500

    Bro Code numpy printing out our version of numpy

--- numpy_startup.py
+++ numpy_startup.py
@@ -6,3 +6,8 @@
 # EXAMPLE
 # np.array([1, 2, 3]) * 2 = [2, 4, 6] - uses numpy
 # [1, 2, 3] * 2 = [1, 2, 3, 1, 2, 3] - uses regular python
+
+import numpy as np
+
+# prints current version of numpy
+print(np.__version__)
