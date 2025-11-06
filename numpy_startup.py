ece685a10583789dfa55a1d8e1fb6fc745d9b769
william <espiw@farmingdale.edu>
2025-11-05 20:10:36 -0500

    Bro Code numpy beginner notes

---
+++ numpy_startup.py
@@ -0,0 +1,8 @@
+# numpy stands for numerical python
+# its a python library used for numerical computing
+# wildly used in fields like data science, engineering, AI/ML
+# under the hood, numpy is written in c
+
+# EXAMPLE
+# np.array([1, 2, 3]) * 2 = [2, 4, 6] - uses numpy
+# [1, 2, 3] * 2 = [1, 2, 3, 1, 2, 3] - uses regular python
