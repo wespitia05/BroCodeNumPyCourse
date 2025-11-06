6d13bb68b12409cc9d1480cb1ef8dc60ca48112d
william <espiw@farmingdale.edu>
2025-11-05 20:31:21 -0500

    Bro Code numpy printing out the shape of the arrays

--- numpy_multidimensional_arrays.py
+++ numpy_multidimensional_arrays.py
@@ -4,16 +4,21 @@ import numpy as np
 
 array0 = np.array('A') # zero dimensional array
 print(array0.ndim) # prints number of dimensions
+print(array0.shape) # prints tuple of integers (depth, rows, columns)
+
 
 array1 = np.array(['A', 'B', 'C']) # one dimensional array
 print(array1.ndim) # prints number of dimensions
+print(array1.shape) # prints tuple of integers (depth, rows, columns)
 
 array2 = np.array([['A', 'B', 'C'], 
                    ['D', 'E', 'F'],
                    ['G', 'H', 'I']]) # two dimensional array
 print(array2.ndim) # prints number of dimensions
+print(array2.shape) # prints tuple of integers (depth, rows, columns)
 
 array3 = np.array([[['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']],
                    [['J', 'K', 'L'], ['M', 'N', 'O'], ['P', 'Q', 'R']],
                    [['S', 'T', 'U'], ['V', 'W', 'X'], ['Y', 'Z', '_']]]) # three dimensional array
 print(array3.ndim) # prints number of dimensions
+print(array3.shape) # prints tuple of integers (depth, rows, columns)
