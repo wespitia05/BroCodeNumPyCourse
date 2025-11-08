bd2ad9198767699ed0739c08242f3e073c2df09f
william <espiw@farmingdale.edu>
2025-11-08 13:05:41 -0500

    Bro Code numpy slicing pt 3

--- numpy_slicing.py
+++ numpy_slicing.py
@@ -16,6 +16,7 @@ print(array[2])
 # accessing array at index 3
 print(array[3])
 
+# ***** ROW SELECTION ***** #
 # access 1-12
 print(array[0:3])
 # access 5-16
@@ -26,3 +27,21 @@ print(array[0:])
 print(array[0:4:2]) # end point is exclusive
 # access 5-8, 13-16
 print(array[1:5:2]) # end point is exclusive
+# access 1-16 in reverse
+print(array[::-1])
+# access 5-8, 13-16 in reverse
+print(array[::-2])
+
+# ***** COLUMN SELECTION ***** #
+# selecting all rows and accessing column 0
+print(array[:, 0])
+# selecting all rows and accessing column 1
+print(array[:, 1])
+# selecting all rows and accessing column 2
+print(array[:, 2])
+# selecting all rows and accessing column 3
+print(array[:, 3])
+# selecting all rows and accessing column 1, 2, 3
+print(array[:, 0:3]) # end point is exclusive
+# selecting all rows and accessing column 2, 3, 4
+print(array[:, 1:4]) # end point is exclusive
