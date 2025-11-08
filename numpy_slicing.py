9794e2f34fadbb35d6d873d1793bd1009c8d6e46
william <espiw@farmingdale.edu>
2025-11-08 12:56:45 -0500

    Bro Code numpy slicing pt 1

---
+++ numpy_slicing.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+array = np.array([[1, 2, 3, 4], 
+                  [5, 6, 7, 8], 
+                  [9, 10, 11, 12], 
+                  [13, 14, 15, 16]])
+
+# array[start:end:step]
+
+# accessing array at index 0
+print(array[0])
+# accessing array at index 1
+print(array[1])
+# accessing array at index 2
+print(array[2])
+# accessing array at index 3
+print(array[3])
