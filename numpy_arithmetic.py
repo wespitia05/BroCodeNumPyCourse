ef35223a4da3b03363dc36ab795fd9d5ea99eefa
william <espiw@farmingdale.edu>
2025-11-09 13:50:04 -0500

    Bro Code numpy vectorized math functions

--- numpy_arithmetic.py
+++ numpy_arithmetic.py
@@ -8,3 +8,12 @@ print(array - 2)
 print(array * 3)
 print(array / 4)
 print(array ** 5)
+
+# ***** VECTORIZED MATH FUNCTIONS ***** #
+array1 = np.array([1.01, 2.5, 3.99])
+
+print(np.sqrt(array1))
+print(np.round(array1))
+print(np.floor(array1)) # always round down
+print(np.ceil(array1)) # always round up
+print(np.pi)
