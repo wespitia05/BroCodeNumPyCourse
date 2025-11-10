c51b9b9a821837e9dd6481a93a9e0f5023e17e49
william <espiw@farmingdale.edu>
2025-11-09 21:37:50 -0500

    Bro Code numpy filtering ages into a new array

---
+++ numpy_filtering.py
@@ -0,0 +1,11 @@
+# filtering = refers to the process of selecting elements from
+#             an array that match a given condition
+
+import numpy as np
+
+ages = np.array([[21, 17, 19, 20, 16, 30, 18, 65], 
+                 [39, 22, 15, 99, 18, 19, 20, 21]])
+
+# create new array with teenagers
+teenagers = ages[ages < 18]
+print(teenagers)
