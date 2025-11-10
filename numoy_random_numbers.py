bcd68db09ca8e6b02dc6e3152839e4edf1dbb939
william <espiw@farmingdale.edu>
2025-11-09 21:52:13 -0500

    Bro Code numpy random numbers pt 2

--- numoy_random_numbers.py
+++ numoy_random_numbers.py
@@ -6,3 +6,11 @@ rng = np.random.default_rng()
 # prints the random numbers
 # parameters: starting, end (exclusive), size
 print(rng.integers(low=1, high=101, size=(3, 2)))
+
+# uniform = uniform distribution (floating point number)
+print(np.random.uniform(low=-1, high=1, size=(3,3)))
+
+# shuffling an array
+array = np.array([1, 2, 3, 4, 5])
+rng.shuffle(array)
+print(array)
